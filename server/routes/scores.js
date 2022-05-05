e3d20319227719b9dfad4c1a9e2b01d07e575578
scott.shevrin <scott.shevrin@protegrity.com>
2022-05-05 15:35:15 -0400

    init

--- server/routes/scores.js
+++ server/routes/scores.js
@@ -1,5 +1,17 @@
-module.exports = app => {
-    app.get('scores', (req, res) => {
-        res.send('scores');
-    })
-}
+const express = require('express')
+const router = express.Router();
+const bodyParser = require("body-parser");
+
+// create application/json parser
+let jsonParser = bodyParser.json()
+
+router.get('/', (req, res) => {
+    res.send('scores');
+});
+
+router.post('/', jsonParser, (req, res) => {
+    let body = req.body;
+    res.send(body);
+});
+
+module.exports = router;
